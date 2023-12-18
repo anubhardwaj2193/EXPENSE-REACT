0d513e55d45123df301f7fbf013eaa2c9e978760
anubhardwaj2193 <anubhardwaj2193>
2023-12-18 23:59:20 +0530

    signupcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes,redirect ,Router,Route, createRoutesFromChildren, createBrowserRouter, RouterProvider} from "react-router-dom";
+import Login from "./Component/Pages/Login";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '../node_modules/react-bootstrap/dist/react-bootstrap'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import HeaderSite from "./Component/UI/HeaderSite";
 
+
+const routeDefinitions = createRoutesFromChildren(
+  <Route>
+    <Route path="/" element={<HeaderSite/>} children={[
+      <Route path="login" element={<Login/>}/>
+    ]}></Route>
+  </Route>
+)
+
+const router = createBrowserRouter(routeDefinitions)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+     <RouterProvider router={router}/>
+     <Login/>
+
+   </React.Fragment>
   );
 }
-
 export default App;
